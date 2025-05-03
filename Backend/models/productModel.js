785a1860613e5d5948a9bf44ef1c47c0f3f4433d
Sudip Shrestha <sudip10k89@gmail.com>
2025-05-03 18:08:38 +0545

    Add product and user models with schemas for MongoDB

---
+++ Backend/models/productModel.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const productSchema = new moongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: Array,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  subCategory: {
+    type: String,
+    required: true,
+  },
+  sizes: {
+    type: Array,
+    required: true,
+  },
+  bestseller: {
+    type: Boolean,
+    default: false,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const productModel =mongoose.models.product || mongoose.model("Product", productSchema);
+
+export default productModel;
