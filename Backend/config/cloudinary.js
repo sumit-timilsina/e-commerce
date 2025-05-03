f481f47d7b7d2e70ed7222830f654f4b0defe023
Sudip Shrestha <sudip10k89@gmail.com>
2025-05-03 17:09:15 +0545

    Add server configuration and database connection setup

---
+++ Backend/config/cloudinary.js
@@ -0,0 +1,17 @@
+import {v2 as cloudinary} from 'cloudinary';
+
+//connect cloudinary
+
+export const connectCloudinary = async () => {
+    try {
+        cloudinary.config({
+            cloud_name: process.env.CLOUDINARY_NAME,
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_SECRET_KEY,
+        });
+        console.log('Cloudinary Connected');
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+}
--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,28 @@
-console.log("Server is running...");
-
 import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+import connectDB from "./config/monogdb.js";
+import { connectCloudinary } from "./config/cloudinary.js";
 
+
+//App Config
 const app = express();
+const PORT = 3000||process.env.PORT;
+
+connectDB();
+connectCloudinary();
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+
+//api endpoints
 
 app.get("/", (req, res) => {
     res.send("Welcome to the e-commerce API!");
 });
 
-const PORT = 3000;
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
